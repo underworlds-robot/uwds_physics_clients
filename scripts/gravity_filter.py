34faea0a22a749ace5b24ea1fe78cd67ee4728f3
Yoan Sallami <ysallami@laas.fr>
2018-11-22 23:54:30 +0100

    :ambulance: fix clients

--- scripts/gravity_filter.py
+++ scripts/gravity_filter.py
@@ -7,7 +7,10 @@ import pybullet_data
 from pyuwds.reconfigurable_client import ReconfigurableClient
 from uwds_msgs.msg import Changes
 from uwds_msgs.srv import *
-from pyuwds.types import FILTER
+from pyuwds.types import FILTER, MESH
+import tf_conversions
+import geometry_msgs
+import tf
 
 
 class GravityFilter(ReconfigurableClient):
@@ -15,35 +18,28 @@ class GravityFilter(ReconfigurableClient):
         """
         """
         self.ressource_folder = rospy.get_param("~ressource_folder")
-        self.time_step = rospy.get_param("~time_step")
-        self.simulation_step = rospy.get_param("~simulation_step")
-
+        self.time_step = rospy.get_param("~time_step", 0.01)
+        self.simulation_step = rospy.get_param("~simulation_step", 1.0)
         ReconfigurableClient.__init__(self, "gravity_filter", FILTER)
 
     def onReconfigure(self, worlds_names):
         """
         """
-        if len(worlds_names) > 1:
-            rospy.logerr("[%s] This filter have only one input. Ignoring the others.", self.node_name)
-        world_name = worlds_names[0]
-        p.connect(p.DIRECT) # Initialize bullet non-graphical version
-        #rospy.loginfo("add search path :"+pybullet_data.getDataPath())
-        #p.setAdditionalSearchPath(pybullet_data.getDataPath())
-        p.setGravity(0, 0, -9.8)
-        #rospy.loginfo("add search path :"+self.ressource_folder)
-        p.setAdditionalSearchPath(self.ressource_folder)
-        p.setTimeStep(self.time_step)
-        self.node_id_map = {}
-        self.reverse_node_id_map = {}
-        planeId = p.loadURDF("plane.urdf")
-        self.node_id_map[self.worlds[world_name].scene.rootID] = planeId
-        self.reverse_node_id_map[planeId] = self.worlds[world_name].scene.rootID
         pass
 
     def onSubscribeChanges(self, world_name):
         """
         """
-        pass
+        p.connect(p.DIRECT) # Initialize bullet non-graphical version
+        p.setGravity(0, 0, -10)
+        p.setAdditionalSearchPath(self.ressource_folder)
+        p.setTimeStep(self.time_step)
+        self.urdf_available = {}
+        self.node_id_map = {}
+        self.reverse_node_id_map = {}
+        planeId = p.loadURDF("plane.urdf")
+        self.node_id_map[self.worlds[world_name].scene.rootID] = planeId
+        self.reverse_node_id_map[planeId] = self.worlds[world_name].scene.rootID
 
     def onUnsubscribeChanges(self, world_name):
         """
@@ -53,51 +49,73 @@ class GravityFilter(ReconfigurableClient):
     def onChanges(self, world_name, header, invalidations):
         """
         """
+        rospy.loginfo("[%s::onChanges] Changes received for world <%s>", self.node_name, world_name)
         changes = self.filter(world_name, header, invalidations)
-        self.sendWorldChanges(world_name+"_stable", header, changes)
+        if len(changes.nodes_to_update) > 0:
+            self.sendWorldChanges(world_name+"_stable", header, changes)
+            rospy.loginfo("[%s::onChanges] Changes send in world <%s>", self.node_name, world_name+"_stable")
 
     def filter(self, world_name, header, invalidations):
         """
         """
         changes = Changes()
-        self.updateOutputNodes(invalidations.nodes_ids_updated)
+        for node_id in invalidations.node_ids_updated:
+            self.updateBulletNodes(world_name, node_id)
         self.stepSimulation()
+        for node_id, node in self.worlds[world_name].scene.nodes.items():
+            if node_id in invalidations.node_ids_updated:
+                if node_id in self.urdf_available:
+                    if self.urdf_available[node_id]:
+                        t, q = p.getBasePositionAndOrientation(self.node_id_map[node_id])
+                        x, y, z = t
+                        node.position.pose.position.x = x
+                        node.position.pose.position.y = y
+                        node.position.pose.position.z = z
+                        x, y, z, w = q
+                        node.position.pose.orientation.x = x
+                        node.position.pose.orientation.y = y
+                        node.position.pose.orientation.z = z
+                        node.position.pose.orientation.w = w
+                if node_id in invalidations.node_ids_updated:
+                    changes.nodes_to_update.append(node)
 
-        for node in self.worlds[world_name].scene.nodes.items():
-            node_id = node.id
-            if node_id in self.node_id_map:
-                if self.node_id_map[node_id] > 0:
-                    t, q = p.getBasePositionAndOrientation(self.node_id_map[node_id])
-                    node.position.pose.position = t
-                    node.position.pose.orientation = q
-                    changes.nodes_to_update.push_back(node)
         return changes
 
-    def stepSimulation(self, simulation_step):
+    def stepSimulation(self):
         """
         """
-        range = self.simulation_step % self.time_step
-        for i in range(0, range):
+        step = int(self.simulation_step / self.time_step)
+        for i in range(0, step):
             p.stepSimulation()
 
-    def updateBulletNodes(self, world_name, nodes_ids):
+    def updateBulletNodes(self, world_name, node_id):
         """ This function load the urdf corresponding to the uwds node and set it in the environment
         The urdf need to have the same name than the node name
         :return:
         """
-        for node_id in nodes_ids:
-            if node_id != self.worlds[world_name].scene().rootID():
+        if node_id != self.worlds[world_name].scene.rootID:
+            node = self.worlds[world_name].scene.nodes[node_id]
+            if node.type == MESH:
+                rospy.loginfo("[%s::updateBulletNodes] Update node <%s(%s)> to bullet", self.node_name, node.name, node_id)
                 position = [node.position.pose.position.x, node.position.pose.position.y, node.position.pose.position.z]
                 orientation = [node.position.pose.orientation.x, node.position.pose.orientation.y, node.position.pose.orientation.z, node.position.pose.orientation.w]
-                if node_id not in node_id_map:
-                    node = input_world.scene.nodes[node_id]
-                    bullet_id = p.loadURDF("urdf/"+node.name+".urdf", position, orientation)
+                if node_id not in self.node_id_map:
+                    try:
+                        bullet_id = p.loadURDF(node.name+".urdf", position, orientation)
+                        self.urdf_available[node_id] = True
+                    except Exception as e:
+                        self.urdf_available[node_id] = False
+                        bullet_id = -1
+                    if self.urdf_available[node_id]:
+                        rospy.loginfo("[%s::updateBulletNodes] "+node.name+".urdf' loaded successfully", self.node_name)
+                        self.reverse_node_id_map[bullet_id] = node_id
                     self.node_id_map[node_id] = bullet_id
-                    self.reverse_node_id_map[bullet_id] = node_id
                 else:
-                    if self.node_id_map[node_id] > 0:
+                    if self.urdf_available[node_id] > 0:
                         p.resetBaseVelocity(self.node_id_map[node_id], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0])
                         p.resetBasePositionAndOrientation(self.node_id_map[node_id], position, orientation)
+            else:
+                self.urdf_available[node_id] = False
 
 
 if __name__ == '__main__':
--- scripts/visibility_monitor.py
+++ scripts/visibility_monitor.py
@@ -1,31 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
 import rospy
 import pybullet as p
 import pybullet_data
 from pyuwds.reconfigurable_client import ReconfigurableClient
 from uwds_msgs.msg import Changes
 from uwds_msgs.srv import *
-from pyuwds import *
+from pyuwds.types import MONITOR, MESH, CAMERA
 import tf
 
 class VisibilityMonitor(ReconfigurableClient):
-    def __init__(self, ressource_folder, simulation_step, global_frame_id, min_treshold, input_world, output_world):
+    def __init__(self):
         """
         """
-        ReconfigurableClient.__init__(self, "visibility_monitor", pyuwds.FILTER)
-        p.connect(p.DIRECT) # Initialize bullet non-graphical version
-        p.setAdditionalSearchPath(pybullet_data.getDataPath())
-        p.setAdditionalSearchPath(ressource_folder)
-
-        self.input_world = input_world
-        self.output_world = output_world
-
-        self.node_id_map = {}
-        self.reverse_node_id_map = {}
-        bullet_id = p.loadURDF("plane.urdf")
-        self.node_id_map[input_world.scene.rootID] = bullet_id
-        self.reverse_node_id_map[bullet_id] = input_world.scene.rootID
-        self.current_situation_map = {}
-        self.min_treshold = min_treshold
+        self.ressource_folder = rospy.get_param("~ressource_folder")
+        ReconfigurableClient.__init__(self, "visibility_monitor", MONITOR)
 
     def onReconfigure(self, worlds):
         """
@@ -35,7 +25,15 @@ class VisibilityMonitor(ReconfigurableClient):
     def onSubscribeChanges(self, world_name):
         """
         """
-        pass
+        p.connect(p.GUI) # Initialize bullet non-graphical version
+        p.setGravity(0, 0, -10)
+        p.setAdditionalSearchPath(self.ressource_folder)
+        self.urdf_available = {}
+        self.node_id_map = {}
+        self.reverse_node_id_map = {}
+        planeId = p.loadURDF("plane.urdf")
+        self.node_id_map[self.worlds[world_name].scene.rootID] = planeId
+        self.reverse_node_id_map[planeId] = self.worlds[world_name].scene.rootID
 
     def onUnsubscribeChanges(self, world_name):
         """
@@ -45,63 +43,68 @@ class VisibilityMonitor(ReconfigurableClient):
     def onChanges(self, world_name, header, invalidations):
         """
         """
-        changes = self.monitor(invalidations)
-        self.sendWorldChanges(world_name+"_stable", header, changes)
+        rospy.loginfo("[%s::onChanges] Changes received for world <%s>", self.node_name, world_name)
+        changes = self.monitor(world_name, header, invalidations)
+        if len(changes.situations_to_update) > 0:
+            self.sendWorldChanges("visibilities", header, changes)
+            rospy.loginfo("[%s::onChanges] Changes send in world <%s>", self.node_name, world_name+"_stable")
 
-    def monitor(self, input_invalidations):
+    def monitor(self, world_name, header, invalidations):
         """
         """
         changes = Changes()
-        self.updateOutputNodes(input_invalidations.nodes_ids_updated)
-        self.stepSimulation()
+        for node_id in invalidations.node_ids_updated:
+            self.updateBulletNode(world_name, node_id)
 
-        for node in input_world.scene.nodes.items():
-            node_id = node.id
-            if node_id in self.node_id_map:
-                if self.node_id_map[node_id] > 0:
-                    t, q = p.getBasePositionAndOrientation(self.node_id_map[node_id])
-                    node.position.pose.position = t
-                    node.position.pose.orientation = q
-                    changes.nodes_to_update.push_back(node)
+        for node_id, node in self.worlds[world_name].scene.nodes.items():
+            if node_id in invalidations.node_ids_updated:
+                if node.type == CAMERA:
+                    visibilities = computeVisibilities(world_name, camera_id)
+                    print visibilities
+                    situations = computeSituations(world_name, header.stamp, node_id, visibilities)
+                    for situation in situations:
+                        changes.situations_to_update.append(situation)
+                changes.nodes_to_update.append(node)
         return changes
 
-    def computeVisibilities(self, camera_id):
+    def computeVisibilities(self, world_name, camera_id):
         visibilities = {}
         if not p.isNumpyEnabled():
-            rospy.logwarn("Numpy not enabled, copy of image buffer can be slow")
-        if camera_id in input_world.scene.nodes:
-            camera_node = input_world.scene.nodes[camera_id]
+            rospy.logwarn("[%s::computeVisibilities] Numpy not enabled, copy of image buffer can be slow", self.node_name)
+        if camera_id in self.worlds[world_name].scene.nodes:
+            camera_node = self.worlds[world_name].scene.nodes[camera_id]
             position = [camera_node.position.pose.position.x, camera_node.position.pose.position.y, camera_node.position.pose.position.z]
             euler = tf.transformations.euler_from_quaternion(camera_node.position.pose.orientation)
             view_matrix = p.computeViewMatrixFromYawPitchRoll(position, 4, euler[0], euler[1], euler[2], 2)
-            fov = float(input_world.scene.getNodeProperty(camera_id, "hfov"))
-            clipnear = float(input_world.scene.getNodeProperty(camera_id, "clipnear"))
-            clipfar = float(input_world.scene.getNodeProperty(camera_id, "clipfar"))
-            aspect = float(input_world.scene.getNodeProperty(camera_id, "aspect"))
+            fov = float(self.worlds[world_name].scene.getNodeProperty(camera_id, "hfov"))
+            clipnear = float(self.worlds[world_name].scene.getNodeProperty(camera_id, "clipnear"))
+            clipfar = float(self.worlds[world_name].scene.getNodeProperty(camera_id, "clipfar"))
+            aspect = float(self.worlds[world_name].scene.getNodeProperty(camera_id, "aspect"))
             proj_matrix = p.computeProjectionMatrixFOV(fov, aspect, clipnear, clipfar)
-            img = p.getCameraImage(self.width, self.height, flagsb = p.ER_SEGMENTATION_MASK_OBJECT_AND_LINKINDEX)
+            img = p.getCameraImage(self.width, self.height, flags = p.ER_SEGMENTATION_MASK_OBJECT_AND_LINKINDEX)
             seg = img[4]
-            cv_image = pyopencv.Mat(height, width)
+            #cv_image = pyopencv.Mat(height, width)
             for pixel in seg:
                 if (pixel >= 0):
                     bullet_id = pixel & ((1 << 24)-1)
                     uwds_id = self.reverse_node_id_map[bullet_id]
-                    cv_image[width * height+1] = pixel
+                    #cv_image[width * height+1] = pixel
                     if uwds_id not in visibilities:
                         visibilities[uwds_id] = 1
                     else:
                         visibilities[uwds_id] += 1
                 for visibility_score in visibilities.items():
                     visibility_score / self.width * self.height
-        return visibilities, cv_image
+        return visibilities#, cv_image
 
-    def computeSituations(self, stamp, camera_id, visibilities):
+    def computeSituations(self, world_name, stamp, camera_id, visibilities):
         situations = []
         for id_seen, visibility_score in visibilities:
-            if camera_id+id_seen not in self.current_situation_map:
+            if camera_id+id_seen not in self.current_situation_map and visibility_score > self.min_treshold:
                 situation = Situation()
                 situation.id = str(uuid.uuid4())
-                situation.description = input_world.scene.nodes[id_seen].name + "is visible by" input_world.scene.nodes[camera_id].name
+                situation.type = FACT
+                situation.description = self.worlds[world_name].scene.nodes[id_seen].name + "is visible by" + self.worlds[world_name].scene.nodes[camera_id].name
                 predicate = Property()
                 predicate.name = "predicate"
                 predicate.data = "isVisible"
@@ -117,36 +120,47 @@ class VisibilityMonitor(ReconfigurableClient):
                 situation.confidence = visibility_score
                 situation.start.data = stamp
                 self.current_situation_map[camera_id+entity_seen] = situation
+                situations.append(situation)
             else:
                 situation = self.current_situation_map[camera_id+entity_seen]
                 situation.confidence = visibility_score
                 if situation.confidence < self.min_treshold:
                     situation.end = stamp
                     del self.current_situation_map[camera_id+entity_seen]
-            situations.append(situation)
+                situations.append(situation)
         return situations
 
-    def updateBulletNodes(self, nodes_ids):
+    def updateBulletNode(self, world_name, node_id):
         """ This function load the urdf corresponding to the uwds node and set it in the environment
         The urdf need to have the same name than the node name
         :return:
         """
-        for node_id in nodes_ids:
-            if node_id != input_world.scene().rootID():
+        if node_id != self.worlds[world_name].scene.rootID:
+            node = self.worlds[world_name].scene.nodes[node_id]
+            if node.type == MESH:
+                rospy.loginfo("[%s::updateBulletNodes] Update node <%s(%s)> to bullet", self.node_name, node.name, node_id)
                 position = [node.position.pose.position.x, node.position.pose.position.y, node.position.pose.position.z]
                 orientation = [node.position.pose.orientation.x, node.position.pose.orientation.y, node.position.pose.orientation.z, node.position.pose.orientation.w]
-                if node_id not in node_id_map:
-                    node = input_world.scene.nodes[node_id]
-                    bullet_id = p.loadURDF("urdf/"+node.name+".urdf", position, orientation)
+                if node_id not in self.node_id_map:
+                    try:
+                        bullet_id = p.loadURDF(node.name+".urdf", position, orientation)
+                        self.urdf_available[node_id] = True
+                    except Exception as e:
+                        self.urdf_available[node_id] = False
+                        bullet_id = -1
+                    if self.urdf_available[node_id]:
+                        rospy.loginfo("[%s::updateBulletNodes] "+node.name+".urdf' loaded successfully", self.node_name)
+                        self.reverse_node_id_map[bullet_id] = node_id
                     self.node_id_map[node_id] = bullet_id
-                    self.reverse_node_id_map[bullet_id] = node_id
                 else:
-                    if self.node_id_map[node_id] > 0:
+                    if self.urdf_available[node_id] > 0:
                         p.resetBaseVelocity(self.node_id_map[node_id], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0])
                         p.resetBasePositionAndOrientation(self.node_id_map[node_id], position, orientation)
+            else:
+                self.urdf_available[node_id] = False
 
 
 if __name__ == '__main__':
     rospy.init_node("visibility_monitor")
-    gf = GravityFilter()
+    vm = VisibilityMonitor()
     rospy.spin()
